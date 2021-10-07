72f354fea703fcc000b65a89cc39da270729bd0c
okadate <okadate@gmail.com>
2021-10-07 18:59:33 +0900

    fix a dateparser bug in HOBO.py & change HOBO.py to parsee DO loger

--- HOBO.py
+++ HOBO.py
@@ -3,30 +3,30 @@ import pandas as pd
 
 def dateparser(inp):
     date = pd.to_datetime(inp[:-3])
-    if date.hour == 12:
-        return date
-    if inp.split()[-1] == u'午後':
+    ampm = inp.split()[-1]
+    if ampm == u'午前' and date.hour == 12:
+        date -= pd.Timedelta(hours=12)
+    elif ampm == u'午後' and date.hour != 12:
         date += pd.Timedelta(hours=12)
-        return date
     return date
 
 
 def read_csv(f, raw=False):
-    if raw:
-        df = pd.read_csv(f, skiprows=2, 
-                         names='No date press temp'.split(), 
+    df = pd.read_csv(f, skiprows=1, 
                          date_parser=dateparser, 
-                         parse_dates=['date'],
-                         index_col='date')
-        return df['press temp'.split()]
-    else:
-        df = pd.read_csv(f, skiprows=2, 
-                         names='No date Pw temp Pa depth ID'.split(), 
-                         date_parser=dateparser, 
-                         parse_dates=['date'],
-                         index_col='date')
-        return df['depth ID'.split()]
-
+                         parse_dates=[1],
+                         index_col=1)
+    df.index.name = 'date'
+    names = df.columns.values
+    for i, name in enumerate(names):
+        if 'DO' in name:
+            names[i] = 'DO'
+        elif u'温度' in name:
+            names[i] = 'temp'
+    df.columns = names
+    #df = df.rename(columns={"DO 濃度, mg/L (LGR S/N: 20758290, SEN S/N: 20758290)":'DO',
+    #                    "温度, °C (LGR S/N: 20758290, SEN S/N: 20758290)":'temp'})
+    return df
 
 if __name__ == '__main__':
     assert(dateparser('08/21/20 01:46:22 午後')==pd.to_datetime('2020-08-21 13:46:22'))
